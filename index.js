7f476496b9e3439bdcc2aa716df0472b667a696c
salman <salmanfaris10072005@gmail.com>
2026-05-10 21:19:47 +0700

    benarin docker smga bisa

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const PORT = process.env.PORT || 3000;
 
 sequelize.sync()
     .then(() => {
-        app.listen(PORT, () => {
+        app.listen(PORT,'0,0,0,0', () => {
             console.log(`Server is running on port ${PORT}`);
         });
     })
